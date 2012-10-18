aab16b48379c4a7f2df6e2f999ee7cccca8ff85f
Ionut Bizau <i@ibz.me>
2012-10-18 13:08:06 +0200

    Ignore errors in Columbus files.

--- in_columbus.py
+++ in_columbus.py
@@ -1,20 +1,25 @@
 import datetime
+import sys
 
 import utils
 
 def parse(fileobj, transportation):
     path = {'transportation': transportation, 'points': []}
 
-    for line in fileobj:
+    for i, line in enumerate(fileobj, 1):
         parts = [p.replace("\x00", "") for p in line.split(",")]
 
         if parts[0] == "INDEX": # first line, skip
             continue
 
-        lat = utils.parse_latlon(parts[4])
-        lon = utils.parse_latlon(parts[5])
-        ele = int(parts[6])
-        time = datetime.datetime.strptime(parts[2] + parts[3], "%y%m%d%H%M%S")
+        try:
+            lat = utils.parse_latlon(parts[4])
+            lon = utils.parse_latlon(parts[5])
+            ele = int(parts[6])
+            time = datetime.datetime.strptime(parts[2] + parts[3], "%y%m%d%H%M%S")
+        except:
+            sys.stderr.write("Error on line %s\n" % i)
+            continue
 
         if lat is None or lon is None:
             continue
