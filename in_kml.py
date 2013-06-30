9529821f2a010836297b78d8b9f60d7784aa0fd7
Ionut Bizau <i@ibz.me>
2013-06-30 11:48:28 +0200

    kml fix

--- in_kml.py
+++ in_kml.py
@@ -18,7 +18,7 @@ class KmlHandler(xml.sax.handler.ContentHandler):
     def startElement(self, name, attrs):
         self.xml_path.append(name)
 
-        if name in ['LineString']:
+        if name in ['LineString', 'LinearRing']:
             self.path = {'transportation': self.transportation, 'points': []}
 
     def characters(self, content):
@@ -26,13 +26,13 @@ class KmlHandler(xml.sax.handler.ContentHandler):
         content = content.strip()
         if not content:
             return
-        if tail == ['LineString', 'coordinates']:
+        if tail == ['LineString', 'coordinates'] or tail == ['LinearRing', 'coordinates']:
             self.path['points'] = [utils.Point(lat, lng, ele, None, None) for lng, lat, ele in [map(float, p.split(",")) for p in content.strip().split(" ")]]
 
     def endElement(self, name):
         self.xml_path.pop()
 
-        if name in ['LineString']:
+        if name in ['LineString', 'LinearRing']:
             self.paths.append(self.path)
             self.path = None
 
